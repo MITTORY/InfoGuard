f66b0bb4f91f4f865e409c5e299fbed24843b964
MITTORY <40243680+MITTORY@users.noreply.github.com>
2023-09-22 19:40:35 +0500

    Add files via upload

---
+++ IBZI #3/IBZI #3/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace IBZI__3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Srbija Sans", 12F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(195, 67);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Srbija Sans", 12F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(213, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(195, 67);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 136);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(396, 166);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Засшифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(287, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 47);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(114, 103);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(294, 27);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(155, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 47);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Генерация \r\nключа";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Srbija Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 27);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Srbija Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Тип ключа";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "IBZI #3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ IBZI #3/IBZI #3/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace IBZI__3
+{
+    public partial class Form1 : Form
+    {
+        private string generatedKey;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("128 бит");
+            comboBox1.Items.Add("256 бит");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string plainText = textBox1.Text;
+            string key = generatedKey;
+            int keySizeInBits = comboBox1.SelectedItem.ToString() == "256 бит" ? 256 : 128;
+
+            string encryptedText = Encrypt(plainText, key, keySizeInBits);
+
+            textBox3.Text = encryptedText;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cipherText = textBox3.Text;
+            string key = generatedKey;
+            int keySizeInBits = comboBox1.SelectedItem.ToString() == "256 бит" ? 256 : 128;
+
+            string decryptedText = Decrypt(cipherText, key, keySizeInBits);
+
+            textBox2.Text = decryptedText;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int keySizeInBits = 128; // По умолчанию 128 бит
+            if (comboBox1.SelectedItem.ToString() == "256 бит")
+            {
+                keySizeInBits = 256;
+            }
+
+            byte[] keyBytes = GenerateRandomKey(keySizeInBits / 8); // Передайте размер в байтах
+            generatedKey = BitConverter.ToString(keyBytes).Replace("-", "");
+            textBox4.Text = generatedKey;
+        }
+
+        private byte[] GenerateRandomKey(int keySizeInBytes)
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] key = new byte[keySizeInBytes];
+                rng.GetBytes(key);
+                return key;
+            }
+        }
+
+        private string Encrypt(string plainText, string key, int keySizeInBits)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                // Преобразуем ключ в байтовый массив нужной длины
+                int keySizeInBytes = keySizeInBits / 8;
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+                // Если ключ короче, чем ожидаемая длина, увеличьте его до нужной длины
+                if (keyBytes.Length != keySizeInBytes)
+                {
+                    Array.Resize(ref keyBytes, keySizeInBytes);
+                }
+
+                aesAlg.Key = keyBytes;
+                aesAlg.Mode = CipherMode.CFB;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    byte[] encryptedBytes = msEncrypt.ToArray();
+                    byte[] combinedBytes = new byte[aesAlg.IV.Length + encryptedBytes.Length];
+                    Array.Copy(aesAlg.IV, combinedBytes, aesAlg.IV.Length);
+                    Array.Copy(encryptedBytes, 0, combinedBytes, aesAlg.IV.Length, encryptedBytes.Length);
+
+                    return Convert.ToBase64String(combinedBytes);
+                }
+            }
+        }
+
+        private string Decrypt(string cipherText, string key, int keySizeInBits)
+        {
+            keySizeInBits = keySizeInBits == 256 ? 256 : 128;
+            byte[] combinedBytes = Convert.FromBase64String(cipherText);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Mode = CipherMode.CFB;
+                aesAlg.Padding = PaddingMode.PKCS7;
+
+                // Определите длину ключа в байтах
+                int keySizeInBytes = keySizeInBits / 8;
+
+                // Преобразуйте ключ в байтовый массив нужной длины
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+                // Если ключ короче, чем ожидаемая длина, увеличьте его до нужной длины
+                if (keyBytes.Length != keySizeInBytes)
+                {
+                    Array.Resize(ref keyBytes, keySizeInBytes);
+                }
+
+                aesAlg.Key = keyBytes;
+
+                if (combinedBytes.Length <= aesAlg.IV.Length)
+                {
+                    throw new InvalidOperationException("Недопустимый шифротекст.");
+                }
+
+                byte[] iv = new byte[aesAlg.IV.Length];
+                byte[] encryptedBytes = new byte[combinedBytes.Length - aesAlg.IV.Length];
+                Array.Copy(combinedBytes, iv, iv.Length);
+                Array.Copy(combinedBytes, iv.Length, encryptedBytes, 0, encryptedBytes.Length);
+
+                using (ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, iv))
+                {
+                    using (var msDecrypt = new MemoryStream(encryptedBytes))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (var srDecrypt = new StreamReader(csDecrypt))
+                            {
+                                return srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
